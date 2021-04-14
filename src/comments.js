c3530aef0f66180533bf33d71de94e2f1f3ae810
khckn1 <khckn@gmail.com>
2021-04-14 00:42:57 -0600

    more mock data

--- src/comments.js
+++ src/comments.js
@@ -3,22 +3,40 @@ let comments = {
     {
       id: 1,
       author: "George Washingalot",
-      text: "Good",
+      text: "Very good adventure",
     },
     {
       id: 2,
-      author: "Another",
-      text: "Also good",
+      author: "Tommy Jefferdad",
+      text: "I wish I could have gone",
     },
   ],
 
   1: [
     {
       id: 3,
-      author: "Cool",
-      text: "comment",
+      author: "Alex Beefilton",
+      text: "Really an adventure of a lifetime.",
     },
+    {
+      id: 4,
+      author: "Ben Frankling",
+      text: "An electrifying read.",
+    }
   ],
+
+  2: [
+    {
+      id: 5,
+      author: "Jack Hanrobert",
+      text: "What a big impact this made.",
+    },
+    {
+      id: 6,
+      author: "Jim Madidad",
+      text: "So important, I wish I were there",
+    }
+  ]
 };
 
 export default comments;
